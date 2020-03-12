a5a7e25c3e7faebddfb999b08d28b199ed93ffca
huijie0201 <huijie.shan.2018@smu.edu.sg>
2020-03-12 15:56:10 +0800

    Create insurance_claim.sql

---
+++ sql/insurance_claim.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS Insurance_Claim;
+CREATE DATABASE Insurance_Claim;
+
+CREATE TABLE insurance_claim(
+
+    PatientID int,
+    ClinicID int,
+    ClaimDate DATE,
+    Medicine text,
+    BillAmount FLOAT,
+    ClaimedAmount FLOAT,
+    ClaimStatus VARCHAR(255),
+    RefundStatus VARCHAR(255)
+);
+
+
