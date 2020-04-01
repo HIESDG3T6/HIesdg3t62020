354909b44ced4ca97b2b03d2d95a405ba76434d3
jiamin14 <46833612+jiamin14@users.noreply.github.com>
2020-04-02 01:42:26 +0800

    Docker Image for Notification
    Do not use this first

---
+++ docker/notification/notification.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import requests
+import time
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/telegramNotification/<string:TelegramHandle>/<string:errorMsg>")
+@app.route("/telegramNotification/<string:TelegramHandle>/<string:appointmentDate>/<string:appointmentTime>/<string:clinicName>")
+def sendTelegramNoti(TelegramHandle, appointmentDate=None, appointmentTime=None, clinicName=None, errorMsg=None):
+    getChatID = "http://0.0.0.0:4646/telegram/getChatID/" + TelegramHandle
+    sendToChat = "http://0.0.0.0:4646/telegram/sendMessage/"
+
+    result = requests.get(getChatID).json()
+    chatID = str(result['message'])
+
+    if (errorMsg != None):
+        sendToChat += chatID + "/" + errorMsg
+
+    else:
+        sendToChat += chatID + "/" + appointmentDate + "/" + appointmentTime + "/" + clinicName
+
+    sendResult = requests.get(sendToChat)
+
+    print(sendResult)
+
+    return sendResult
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5566, debug=True)
