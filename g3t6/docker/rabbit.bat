bf6c5985612d14a5b8042b3a98e310e293414aa9
jonathanlxh <jonathanlxh@users.noreply.github.com>
2020-04-10 16:26:59 +0800

    Update rabbit.bat

--- g3t6/docker/rabbit.bat
+++ g3t6/docker/rabbit.bat
@@ -1,2 +1,2 @@
 cd ./refund
-docker run -d --hostname my-rabbit --name some-rabbit -p 8080:15672 rabbitmq:3-management
+docker run -d --hostname my-rabbit --name some-rabbit -p 8080:15672 -p 5672:5672 rabbitmq:3-management
